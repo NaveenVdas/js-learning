81a67e9f5fcd752971992c8880ce3e679ec50c24
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2023-11-22 14:49:24 +0000

    Conversion

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = 33
+
+let valueInNumber = Number(score)
+
+//When converting to number
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+
+// When converting to boolean
+// 1 => true
+// "" => false
+// "Naveen" => true
+// 0 => false
