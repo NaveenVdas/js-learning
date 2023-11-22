274bb3f95e57dc2492338a16e266014cefc63cf8
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2023-11-22 14:23:29 +0000

    variables and constants

---
+++ Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 145654
+let accountEmail = "naveen@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "na@gmail.com"
+accountPassword = "1212"
+accountCity = "Bangalore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
