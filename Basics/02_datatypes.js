e20a1a62ef03f081c609c5344ead7f0151365f79
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2023-11-22 14:29:06 +0000

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let num = 23 //number
+let username = "Naveen" //string
+let isDrunk = false //boolean
+let behaviour = null //null
+let age; //undefined
+let accountBalance = 1234565432134567543213457689087654321457689 //bigint
+
+let credentials = {"userName":"naveen",password:"Naveen@123"} //object
+
+console.table([typeof num,typeof username,typeof isDrunk,typeof behaviour,typeof age,typeof accountBalance,typeof credentials])
