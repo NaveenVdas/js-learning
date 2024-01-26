eee92dc0960cabd12ce4af6e8683624662222432
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2024-01-26 14:49:31 +0000

    Numbers and Maths

---
+++ Basics/05_nums_and_maths.js
@@ -0,0 +1,11 @@
+// const num = 100
+
+// console.log(num.toString());
+
+// ----------------------- Maths ---------------------------------------
+
+console.log(Math.abs(-5));
+
+console.log(Math.round(4.96));
+console.log(Math.ceil(4.96));
+console.log(Math.floor(4.96));
