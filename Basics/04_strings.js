6d233a9790ec6b443db8bd4efe9021b1c7151f80
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2024-01-26 14:22:02 +0000

    strings

---
+++ Basics/04_strings.js
@@ -0,0 +1,28 @@
+const name = "Naveen-dv";
+
+// console.log(name[0]);
+
+// console.log(name.toUpperCase());
+
+// console.log(name.charAt(3));
+// console.log(name.indexOf('e'));
+
+// const newName = name.substring(0,3)
+// console.log(newName);
+
+// const slicedName = name.slice(-4,9);
+// console.log(slicedName);
+
+// const newString = "           Naveen             ";
+// console.log(newString);
+// console.log(newString.trim());
+
+const url = "https://naveen.com/naveen%20dv";
+
+// console.log(url.replace('%20','-'));
+// console.log(url);
+
+// console.log(url.includes('naveen'));
+// console.log(url.includes('naveen',23));
+
+console.log(url.split('/',2));
