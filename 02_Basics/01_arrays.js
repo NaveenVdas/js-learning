a7e7e43ff91205b565b5e88f7fd18d3b7cf0aecf
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2024-01-29 17:48:25 +0000

    Arrays basics

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr = [0,1,2,3,4,5]
+
+const myArr2 = new Array(1,2,3,4);
+
+// myArr2.push(5)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(7));
+
+// const newArr = myArr.join()
+
+// console.log(newArr);
+// console.log(myArr);
+
+//slice ,  splice
+
+const arr1 = myArr.slice(1,3)
+
+const arr2 = myArr.splice(1,3)
+
+console.log(arr1);
+
+console.log(arr2);
