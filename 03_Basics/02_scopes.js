7e799a904e82125428e69cb2b2579d334b3db0fb
NaveenVdas <121152594+NaveenVdas@users.noreply.github.com>
2024-01-30 05:15:48 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,14 @@
+function one(){
+    const username = "Naveen"
+
+    function two(){
+        const website="youtube"
+
+        console.log(username);
+    }
+
+    two()
+    //console.log(website);
+}
+
+one()
